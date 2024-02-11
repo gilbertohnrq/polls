3cee05860f1a28d2ab04d06b371b24fd1bdadb62
Gilberto Henrique <gilbertohnrq@gmail.com>
2024-02-10 23:43:02 -0300

    Add WebSocket support and Redis integration***
    ***Update get-poll route to include poll results***
    
    ***Update vote-on-poll route to publish voting events

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,12 @@
 import fastify from 'fastify';
 import cookie from '@fastify/cookie';
+import websocket from '@fastify/websocket';
 import { z } from 'zod';
 import { prisma } from '../lib/prisma';
 import { createPoll } from './routes/create-poll';
 import { getPoll } from './routes/get-poll';
 import { voteOnPoll } from './routes/vote-on-poll';
+import { pollResults } from './ws/poll-results';
 
 const app = fastify();
 
@@ -13,9 +15,13 @@ app.register(cookie, {
 	hook: 'onRequest',
 });
 
+app.register(websocket);
+
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+
+app.register(pollResults);
 
 app.listen({ port: 3333 }).then(() => {
 	console.log('Server is running on port 3333');
